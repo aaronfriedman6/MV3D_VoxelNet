2d81059b92921199034092601ec10b74f8dddaa7
Aaron Friedman <aaronfriedman6@gmail.com>
2018-12-03 19:08:44 -0500

    first attempt at integrating voxelnet into mv3d

--- src/net/blocks.py
+++ src/net/blocks.py
@@ -327,3 +327,32 @@ def linear_dropout_relu(input, keep=1.0, num_hiddens=1, name='linear_dropout_rel
 
 
 
+class VFELayer(object):
+    
+    def __init__(self, out_channels, name):
+        super(VFELayer, self).__init__()
+        self.units = int(out_channels / 2)
+        with tf.variable_scope(name, reuse=tf.AUTO_REUSE) as scope:
+            self.dense = tf.layers.Dense(
+                self.units, tf.nn.relu, name='dense', _reuse=tf.AUTO_REUSE, _scope=scope)
+            self.batch_norm = tf.layers.BatchNormalization(
+                name='batch_norm', fused=True, _reuse=tf.AUTO_REUSE, _scope=scope)
+
+    def apply(self, inputs, mask, training):
+        # [K, T, 7] tensordot [7, units] = [K, T, units]
+        pointwise = self.batch_norm.apply(self.dense.apply(inputs), training)
+
+        #n [K, 1, units]
+        aggregated = tf.reduce_max(pointwise, axis=1, keep_dims=True)
+
+        # [K, T, units]
+        repeated = tf.tile(aggregated, [1, cfg.VOXEL_POINT_COUNT, 1])
+
+        # [K, T, 2 * units]
+        concatenated = tf.concat([pointwise, repeated], axis=2)
+
+        mask = tf.tile(mask, [1, 1, 2 * self.units])
+
+        concatenated = tf.multiply(concatenated, tf.cast(mask, tf.float32))
+
+        return concatenated
